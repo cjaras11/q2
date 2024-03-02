659d1f7e784297cd3d7d5b63fa818f244e233fcb
unknown <fbthadmin@SD2-PW03KDEB.TLD-AD.COM>
2024-03-02 14:30:40 +0700

    Compare Version 1 and Version 2 from input.

--- src/com/cimb/assignment/q2/Q2Application.java
+++ src/com/cimb/assignment/q2/Q2Application.java
@@ -3,6 +3,8 @@ package com.cimb.assignment.q2;
 import java.util.Scanner;
 
 public class Q2Application {
+	
+	private final static String VERSION_SEPARATOR = "\\.";
 
 	public static void main(String[] args) {
 		
@@ -21,7 +23,7 @@ public class Q2Application {
 		try {
 			new Q2Application().process(version1, version2);
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		}
 
 	}
@@ -30,7 +32,9 @@ public class Q2Application {
 		
 		// Validate data.
 		validateData(version1, version2);
-			
+		
+		// Compare Version 1 and Version 2.
+		System.out.println("Result is " + compareVersion(version1, version2));
 	}
 	
 	/**
@@ -68,6 +72,48 @@ public class Q2Application {
 	 */
 	private boolean isValidVersionPattern(String data) {
 		return data.matches("[0-9]+(\\.[0-9]+)*");
+	}
+	
+	/**
+	 * 
+	 * @param version1
+	 * @param version2
+	 * @return -1 if Version 1 < Version 2, 1 if Version 1 > Version 2, otherwise 0
+	 */
+	private int compareVersion(String version1, String version2) {
+		
+		// Split Version 1 and Version 2 into array by dot.
+		String[] version1s = version1.split(VERSION_SEPARATOR);
+		String[] version2s = version2.split(VERSION_SEPARATOR);
+		
+		int maxIndex = getMaxIndex(version1s, version2s);
+		
+		// Loop Versions array member by maxIndex variable.
+		for (int i = 0; i < maxIndex; i++) {
+			
+			// Default revision value is 0.
+			int revisionVersion1 = 0;
+			int revisionVersion2 = 0;
+			
+			// Set revision value if exists.
+			if (i < version1s.length) revisionVersion1 = Integer.parseInt(version1s[i]);
+			if (i < version2s.length) revisionVersion2 = Integer.parseInt(version2s[i]);
+			
+			// Compare by revision.
+			if (revisionVersion1 < revisionVersion2) return -1;
+			if (revisionVersion1 > revisionVersion2) return 1;
+		}
+		
+		return 0;
+	}
+	
+	private int getMaxIndex(String[] version1s, String[] version2s) {
+		
+		// If version1s length more than or equal version2s length, return version1s length.
+		if (version1s.length >= version2s.length) return version1s.length;
+		
+		// return version2s length by assume version2s length is more than version1s length.
+		return version2s.length;
 	}
 
 }
